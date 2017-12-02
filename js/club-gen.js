c5a88aacdd4ae5a9cba1a7a06c336307552c0ce1
EphTron <es.90@gmx.de>
2017-12-02 19:52:44 +0100

    removed margins and worked with divs as spacing

--- js/club-gen.js
+++ js/club-gen.js
@@ -16,15 +16,16 @@
 
 var club_path = "";
 var language = "en";
+var selected_club = "-";
 
 function choose_club_typ(lang) {
     language = lang;
     var club_selector = document.getElementById("club-selector");
-    var selected_club = "-";
     var _selection = club_selector.options[club_selector.selectedIndex].value;
 
     if (_selection !== selected_club) {
         selected_club = _selection;
+
         var paths = {};
         paths['knob_fold'] = "Knob";
         paths['grip_fold'] = "Griff";
@@ -473,16 +474,18 @@ function choose_club_typ(lang) {
 
             club_path = paths;
             create_club("loop-grip");
+        } else {
+            console.log("Error - Unknown club");
         }
     }
 }
 
 document.getElementById("club-selector").addEventListener('click', function () {
-    choose_club_typ();
+    choose_club_typ(language);
 }, false);
 
 document.getElementById("club-selector").addEventListener('touchend', function () {
-    choose_club_typ();
+    choose_club_typ(language);
 }, false);
 
 function create_club_part(parent, club_part, name_id, fold, items) {
@@ -500,10 +503,16 @@ function create_club_part(parent, club_part, name_id, fold, items) {
     wrap_swip.className = "swiper-wrapper";
     wrap_swip.id = "club-" + club_part + "-swiper";
 
-    var dir = "img_sml/";
+    var dir = "img/";
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        dir = "img_sml/";
+        console.log("Mobile");
+        alert("Mobile");
+    } else {
+        console.log("Not mobile");
+    }
+
     items.forEach(function (item) {
-        // var club_item_path = get_club_path(club_part, item);
-        console.log("ITEM:", item);
         var final_url = dir + fold + "/" + item;
         var image_string = '<img id="img-club-' + club_part + '" ' +
             'class="club-pics center-block" ' +
@@ -597,7 +606,7 @@ function change_text(swiper, club_part) {
     var image = swiper.container[0].querySelector(".swiper-slide-active").querySelector("img");
     var slash_index = image.src.lastIndexOf("/");
     var point_index = image.src.lastIndexOf("gif");
-    // prepare string for
+    // prepare string
     var club_part_name = image.src.substring(slash_index + 1, point_index - 1).replace(/([a-z](?=[A-Z]))/g, '$1_');
     // get and split item attributes and adjust language
     var splitted_names = adjust_language(club_part_name.split("_"));
@@ -606,6 +615,8 @@ function change_text(swiper, club_part) {
         selected_part_text.innerHTML = splitted_names[1] + " " + splitted_names[2];
     } else if (splitted_names.length === 4){
         selected_part_text.innerHTML = splitted_names[1] + " " + splitted_names[2] + " " + splitted_names[3];
+    } else {
+        console.log("Error: Bad image handling");
     }
 }
 
@@ -645,7 +656,7 @@ function adjust_language(word_list){
             else if (word === "lila"){
                 w = "purple";
             }
-            else if (word === "silver"){
+            else if (word === "silber"){
                 w = "silver";
             }
             else if (word === "tuerkis"){
@@ -666,150 +677,12 @@ function adjust_language(word_list){
             else if (word === "Lang"){
                 w = "Long";
             }
+            else if (word === "Stand"){
+                w = "Std.";
+            }
             adjusted_word_list.push(w);
         });
     }
+    console.log("word list", adjusted_word_list);
     return adjusted_word_list
-}
-
-
-
-function get_club_path(type, name) {
-
-    var knob_path_list = [
-        "11708_KnobDelphin_schwarz.gif",
-        "11709_KnobDelphin_weiß.gif",
-        "18118_KnobDelphin_orange.gif",
-        "18207_KnobDelphin_blau.gif",
-        "18208_KnobDelphin_gruen.gif",
-        "19635_KnobDelphin_rot.gif",
-        "19638_KnobDelphin_pink.gif",
-        "18238_KnobRund_blau.gif",
-        "18239_KnobRund_gruen.gif",
-        "18240_KnobRund_orange.gif",
-        "18209_KnobClassic_blau.gif",
-        "18210_KnobClassic_gruen.gif",
-        "19636_KnobClassic_rot.gif",
-        "19637_KnobClassic_pink.gif"
-    ];
-
-    var griff_path_list = [
-        "11088_GriffDelphin_weiß.gif",
-        "11799_GriffClassic_silber.gif",
-        "11800_GriffClassic_weiß.gif",
-        "16014_GriffClassic_gruen.gif",
-        "16015_GriffClassic_orange.gif",
-        "17779_GriffClassic_gelb.gif",
-        "18147_GriffClassic_blau.gif",
-        "18257_GriffClassic_rot.gif",
-        "18599_GriffClassic_pink.gif",
-        "19487_GriffClassic_lila.gif",
-        "11802_GriffPiro_silber.gif",
-        "11803_GriffPiro_weiß.gif",
-        "15581_GriffPiro_orange.gif",
-        "15582_GriffPiro_gruen.gif",
-        "17269_GriffPiro_pink.gif",
-        "17885_GriffPiro_gelb.gif",
-        "17886_GriffPiro_rot.gif",
-        "17931_GriffPiro_blau.gif",
-        "19236_GriffPiro_lila.gif",
-        "19652_Griff_Loop_weiß.gif",
-        "19653_Griff_Loop_blau.gif",
-        "19654_Griff_Loop_gelb.gif",
-        "19655_Griff_Loop_gruen.gif",
-        "19656_Griff_Loop_tuerkis.gif",
-        "19657_Griff_Loop_orange.gif",
-        "19658_Griff_Loop_pink.gif",
-        "19659_Griff_Loop_rot.gif"
-    ];
-
-    var tape_path_list = [
-        "11091_DelphinRing_schwarz.gif",
-        "18864_DelphinRing_weiß.gif",
-        "11225_Tape_weiß.gif",
-        "11226_Tape_schwarz.gif",
-        "18956_Tape_gelb.gif",
-        "18957_Tape_pink.gif",
-        "18958_Tape_orange.gif",
-        "18959_Tape_gruen.gif",
-        "19438_Tape_blau.gif"
-    ];
-
-    var body_path_list = [
-        "11062_KorpusLang_weiß.gif",
-        "11063_KorpusLang_rot.gif",
-        "11064_KorpusLang_pink.gif",
-        "11065_KorpusLang_gelb.gif",
-        "11066_KorpusLang_gruen.gif",
-        "11067_KorpusLang_lila.gif",
-        "11068_KorpusLang_blau.gif",
-        "11070_KorpusLang_schwarz.gif",
-        "12684_KorpusLang_orange.gif",
-        "11071_KorpusKurz_weiß.gif",
-        "11072_KorpusKurz_rot.gif",
-        "11073_KorpusKurz_pink.gif",
-        "11074_KorpusKurz_gelb.gif",
-        "11075_KorpusKurz_gruen.gif",
-        "11076_KorpusKurz_lila.gif",
-        "11077_KorpusKurz_blau.gif",
-        "12677_KorpusKurz_orange.gif",
-        "11078_KorpusAlba_weiß.gif",
-        "11079_KorpusAlba_rot.gif",
-        "11080_KorpusAlba_pink.gif",
-        "11081_1KorpusAlba_gelb.gif",
-        "11081_KorpusAlba_gelb.gif",
-        "11082_KorpusAlba_gruen.gif",
-        "11083_KorpusAlba_lila.gif",
-        "11084_KorpusAlba_blau.gif",
-        "12676_KorpusAlba_orange.gif",
-        "16887_KorpusMirage_weiß.gif",
-        "16888_KorpusMirage_rot.gif",
-        "16889_KorpusMirage_pink.gif",
-        "16890_KorpusMirage_gelb.gif",
-        "16891_KorpusMirage_gruen.gif",
-        "16892_KorpusMirage_blau.gif",
-        "16893_KorpusMirage_orange.gif",
-        "17701_KorpusGrip_weiß.gif",
-        "17784_KorpusGrip_orange.gif",
-        "17788_KorpusGrip_gruen.gif",
-        "19151_KorpusGrip_blau.gif",
-        "19242_KorpusGrip_pink.gif",
-        "19717_KorpusGrip_tuerkis.gif"
-    ];
-
-    var top_path_list = [
-        "11715_1TopClassic_schwarz.gif",
-        "11715_TopClassic_schwarz.gif",
-        "11716_TopClassic_weiß.gif",
-        "18124_TopClassic_orange.gif",
-        "18248_TopClassic_blau.gif",
-        "18249_TopClassic_gruen.gif",
-        "19643_TopClassic_rot.gif",
-        "19644_TopClassic_pink.gif",
-        "11717_TopStand_schwarz.gif",
-        "11718_TopStand_weiß.gif",
-        "18122_TopStand_orange.gif",
-        "18250_TopStand_blau.gif",
-        "18251_TopStand_gruen.gif",
-        "19639_TopStand_rot.gif",
-        "19640_TopStand_pink.gif"
-    ];
-
-    var path_dict = {
-        "knob": knob_path_list,
-        "grip": griff_path_list,
-        "tape": tape_path_list,
-        "body": body_path_list,
-        "top": top_path_list
-    };
-    var val = null;
-    path_dict[type].forEach(function (path) {
-
-        if (path.indexOf(name) !== -1) {
-            console.log(path, name);
-            val = (path).toString();
-
-        }
-    });
-    return val;
 }
